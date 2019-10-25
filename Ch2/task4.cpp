5404723b01dba13e91b00730cdc8da37e4f4cd9c
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2019-10-25 21:19:51 +0300

    Update task4.cpp

--- Ch2/task4.cpp
+++ Ch2/task4.cpp
@@ -6,6 +6,6 @@ int main()
 	cout << "VVedite vozrast: " << endl;
 	int x;
 	cin >> x;
-	cout << "your age in mounts: " << x * 12 << endl;
+	cout << "Your age in mounts: " << x * 12 << endl;
 	return 0;
 }
