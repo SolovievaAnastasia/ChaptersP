c62653ee84a4448886e75db6bcee5d6ef1f162b1
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2019-10-25 22:06:59 +0300

    Update task5.cpp

--- Ch3/task5.cpp
+++ Ch3/task5.cpp
@@ -1,14 +1,13 @@
-#include "stdafx.h"
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	long long t1, t;
+	double t1, t;
 	cout << "First enter word pop" << endl;
 	cin >> t1;
 	cout << "Second enter rus pop" << endl;
 	cin >> t;
-	cout << "finaly " << (t / t1)*100 << "% of world pop" << endl;
+	cout << "Finaly rus pop = " << (t / t1)*100 << "% of world pop" << endl;
 	return 0;
 }
