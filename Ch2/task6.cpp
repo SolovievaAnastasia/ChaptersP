33a20f4816e9cd902fcf3ca932321c39dd7dd5c9
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2019-10-25 21:24:31 +0300

    Update task6.cpp

--- Ch2/task6.cpp
+++ Ch2/task6.cpp
@@ -1,16 +1,16 @@
-#include "stdafx.h"
 #include <iostream>
 using namespace std;
 
-int num(int x) {
-	x = x * 63240;
+int num(int x) 
+{
+	x *= 63240;
 	return(x);
 }
 int main()
 {
 	int t;
-	cout << "Enter in light years " << endl;
+	cout << "Enter in light years: " << endl;
 	cin >> t;
-	cout << "in astro units  = " << num(t) << endl;
+	cout << "In astro units  = " << num(t) << endl;
 	return 0;
 }
