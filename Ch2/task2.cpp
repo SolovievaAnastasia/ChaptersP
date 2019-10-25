d98ee0fb684c91fbd38ecd998b65e981d238d45a
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2019-10-25 21:11:13 +0300

    Update task2.cpp

--- Ch2/task2.cpp
+++ Ch2/task2.cpp
@@ -1,12 +1,10 @@
-#include "stdafx.h"
 #include <iostream>
 using namespace std;
-
 int main()
 {
-	cout << "Vvedite rastoyanie v farlongax" << endl;
+	cout << "Vvedite rastoyanie v farlongax:";
 	int x;
 	cin >> x;
-	cout << x * 220 << "yard" << endl;
+	cout << x * 220 << " yard" << endl;
 	return 0;
 }
