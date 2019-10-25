64e3db2333368c7febb59057a1ac7cfbcae01184
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2019-10-25 21:08:08 +0300

    Update task1.cpp

--- Ch2/task1.cpp
+++ Ch2/task1.cpp
@@ -1,10 +1,8 @@
-#include "stdafx.h"
 #include <iostream>
 using namespace std;
-
 int main()
 {
 	cout << "Name:" << "Anastasia"<< endl;
-	cout <<"Adress"<< "brbrbr street" << endl;
+	cout <<"Adress:"<< "brbrbr street" << endl;
 	return 0;
 }
