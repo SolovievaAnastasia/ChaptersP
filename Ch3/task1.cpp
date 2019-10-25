bd57d395dbc6cd117e9d2f82bda066190768037f
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2019-10-25 21:28:36 +0300

    Update task1.cpp

--- Ch3/task1.cpp
+++ Ch3/task1.cpp
@@ -1,13 +1,12 @@
-#include "stdafx.h"
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	double x;
-	cout << "rost v dyamx" << endl;
+	cout << "rost v dyamx " << endl;
 	cin >> x;
 	x = x/12;
-	cout << "rost v futax" << x << endl;
+	cout << "rost v futax " << x << endl;
 	return 0;
 }
