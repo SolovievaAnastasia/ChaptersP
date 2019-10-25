dc758181b23f37eb6a67a029c61cdb1bf2f52819
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2019-10-25 23:11:49 +0300

    Update and rename task7 to task7.cpp

--- Ch4/task7.cpp
+++ Ch4/task7.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 struct Pizza
     {
-        char Name [20];
+        char name [20];
         float ves,diam;
     };
 int main ()
 {   using namespace std;
     Pizza Pie;
     cout<<"Enter Name:"<<endl;
-    cin.getline (Pie.Name,20);
+    cin.getline (Pie.name,20);
     cout<<"Enter diametr:"<<endl;
     cin>>Pie.diam;
     cout<<"Enter ves:"<<endl;
     cin>>Pie.ves;
-    cout<<"Pizza: "<<Pie.Name<<endl<<"Diametr: "<<Pie.diam<<endl<<"Ves: "<<Pie.ves<<endl;
+    cout<<"Pizza: "<<Pie.name<<endl<<"Diametr: "<<Pie.diam<<endl<<"Ves: "<<Pie.ves<<endl;
     cin.get();
     return 0;
   }
