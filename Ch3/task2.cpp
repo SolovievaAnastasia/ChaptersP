32650e414a70759303f3751bacfb789660ea1100
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2019-10-25 21:36:59 +0300

    Update task2.cpp

--- Ch3/task2.cpp
+++ Ch3/task2.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include <iostream>
 #include <math.h>
 using namespace std;
@@ -9,10 +8,10 @@ int main()
 	cout << "rost v dyamx" << endl;
 	cin >> x1;
 	cout << "rost v futax" << endl;
-  cin >> x2;
-  cout << "ves v funtax"<< endl;
-  cin>> x3;
-  imt = (x3/2.2)/sqr((x2*12+x1)*0.0254);
-  cout<<"IMT ="<<imt<<endl;
-	return 0;
+	cin >> x2;
+	cout << "ves v funtax"<< endl;
+	cin>> x3;
+	imt = (x3/2.2)/pow((x2*12+x1)*0.0254,2);
+	cout<<"IMT ="<<imt<<endl;
+return 0;
 }
