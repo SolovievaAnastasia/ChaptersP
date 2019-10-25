7db0e8ea281e4f66fc683f123829655e69d06c7a
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2019-10-25 21:22:19 +0300

    Update task5.cpp

--- Ch2/task5.cpp
+++ Ch2/task5.cpp
@@ -1,16 +1,16 @@
-#include "stdafx.h"
 #include <iostream>
 using namespace std;
 
-double temp(int t) {
+double temp(int t) 
+{
 	t = 1.8*t + 32;
 	return(t);
 }
 int main()
 {
 	int t;
-	cout << "Enter temp in Cels " << endl;
+	cout << "Enter temp in Cels: " << endl;
 	cin >> t;
-	cout << "temp in far =" << temp(t) << endl;
+	cout << "Temp in far = " << temp(t) << endl;
 	return 0;
 }
